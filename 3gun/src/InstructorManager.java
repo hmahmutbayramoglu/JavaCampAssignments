da04e787b86edad34dea4cf03862003c0e4acb21
Mahmut Hüseyin Bayramoğlu <59068077+hmahmutbayramoglu@users.noreply.github.com>
2021-05-04 03:28:12 +0300

    Update InstructorManager.java

--- 3gun/src/InstructorManager.java
+++ 3gun/src/InstructorManager.java
@@ -1,10 +1,7 @@
 
 public class InstructorManager extends UserManager {
 
-	
-	public void courseAdd(Instructor instructor) {
-		System.out.println(instructor.getEducationCategoryId() +" "+"course has been added to this category");
-	}
+
 	
 	public void addHomeWork() {
 		System.out.println("added HomeWork");
